ce8033417c377f52cc27610a9d59aac097252e00
dami <dami0804@naver.com>
2022-08-19 17:35:28 +0900

    talkfrag 이미지 넣기

---
+++ app/src/main/java/com/dami/lifestyle/board/BoardWrtieActivity.kt
@@ -0,0 +1,12 @@
+package com.dami.lifestyle.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dami.lifestyle.R
+
+class BoardWrtieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_wrtie)
+    }
+}
