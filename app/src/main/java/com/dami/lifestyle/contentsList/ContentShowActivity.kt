54a7949d441b985b963761279e8d6b1579f0fd9d
dami <dami0804@naver.com>
2022-08-23 15:21:08 +0900

    웹 URL로 이동

--- app/src/main/java/com/dami/lifestyle/contentsList/ContentShowActivity.kt
+++ app/src/main/java/com/dami/lifestyle/contentsList/ContentShowActivity.kt
@@ -2,6 +2,9 @@ package com.dami.lifestyle.contentsList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import com.dami.lifestyle.R
 
@@ -11,7 +14,15 @@ class ContentShowActivity : AppCompatActivity() {
         setContentView(R.layout.activity_content_show)
 
 
-       val getUrl = intent.getStringExtra("url")
-        Toast.makeText(this, getUrl,Toast.LENGTH_SHORT).show()
+        val getUrl = intent.getStringExtra("url")
+        Toast.makeText(this, getUrl, Toast.LENGTH_SHORT).show()
+        val webView: WebView = findViewById(R.id.webView) //webview찾기
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true
+        }
+        webView.loadUrl(getUrl.toString())
     }
 }
