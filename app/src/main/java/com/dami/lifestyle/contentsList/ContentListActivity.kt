dee5e7f4da390301f7e916b61122d9dc29ad521d
dami <dami0804@naver.com>
2022-08-25 17:01:18 +0900

    데이터저장

--- app/src/main/java/com/dami/lifestyle/contentsList/ContentListActivity.kt
+++ app/src/main/java/com/dami/lifestyle/contentsList/ContentListActivity.kt
@@ -9,11 +9,17 @@ import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dami.lifestyle.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class ContentListActivity : AppCompatActivity() {
+    //1.items.add해서 데이터를 다 넣은뒤 2.add 코트를 지우고 3.items에 넣어진 데이터 불러오기
+    val items = ArrayList<ContentModel>()
     var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,18 +27,88 @@ class ContentListActivity : AppCompatActivity() {
 
         val database : FirebaseDatabase = FirebaseDatabase.getInstance()
         val myRef = database.getReference("contents")
-        /*myRef.setValue(
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+            //Log.d("이이거2",dataSnapshot.toString()) //데이터 제대로 나옴 확인
+            //데이터 하나씩 빼오기
+                for(dataModel in dataSnapshot.children){
+                    Log.d("이이거2",dataSnapshot.toString())
+                   val item= dataModel.getValue(ContentModel::class.java)
+                    items.add(item!!)
+                }
+                Log.d("이이거3",items.toString()) //안나옴 -->비동기화문제
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("이이거", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        myRef.addValueEventListener(postListener)
+       /* myRef.push().setValue(
             ContentModel(
                 "title1",
                 "https://postfiles.pstatic.net/MjAyMjA4MTdfMjMz/MDAxNjYwNzI3Mjg1NjE5.ZX4YzSh_sfNojjZ-Rj_V-q7F1VMz52eBsAZ-D2c5kNEg.XTvQeMwH4PwtkUklpOflZaW1GAt-Rc8QqX6I0r4dCt4g.JPEG.dami0804/KakaoTalk_20220817_180616358.jpg?type=w773",
                 "https://blog.naver.com/dami0804/222850928512"
             )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title2",
+                "https://postfiles.pstatic.net/MjAyMjA4MTdfMjky/MDAxNjYwNzI3MzI0OTkw.jKp2tBkE7aybRtLCY07ibHrIYuV01xOa-kVWf7a3fMsg.KqjRv0hG1UibqC29KJRrdCB4deVBGPznYD96Y46O-UAg.JPEG.dami0804/KakaoTalk_20220817_180616358_03.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222850928938"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title3",
+                "https://postfiles.pstatic.net/MjAyMjA4MTdfNTcg/MDAxNjYwNzI3MzUwNjYw.aAntYryYXsDVrwjGyNaxGmuhAczNgeBlloXJoZrrjwUg.gq865VINCv5sPufoy3U7VPuvvWeZ5gjU7uBJgYYBy5cg.JPEG.dami0804/KakaoTalk_20220817_180616358_04.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222850929292"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title4",
+                "https://postfiles.pstatic.net/MjAyMjA4MTdfMjg0/MDAxNjYwNzI3Mzc4MzQ3.iV_kJg7XA_7v4Av12ZBSlzET_du98hQNdX27HOJNlFUg.Nah2LKi9jPHSJ8yElC16KSu3iIZbza4Y8ori-u2WCs0g.JPEG.dami0804/KakaoTalk_20220817_180616358_01.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222850929615"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title5",
+                "https://postfiles.pstatic.net/MjAyMjA4MTdfMTk1/MDAxNjYwNzI3NDAyMTQx.SZ9dYBC_kMLYf8JMUh8a4c-UA74zxc4p9MdogT4S_Q8g.UYHe8gO9kXuwPTWupKyibf_XTqBWzxY-ihys7e1iQLAg.JPEG.dami0804/KakaoTalk_20220817_180616358_02.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222850929965"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title6",
+                "https://postfiles.pstatic.net/MjAyMjA4MThfMjY1/MDAxNjYwODIzMzUxMTY1.75dH61rsGGlwyk99qkZnZx1NmiYpGX_wLg2Hq5SCVyAg.obLEaWzqUYCu4GThLbp97O8rAEv8OIq4ZdSkQNYYtm8g.JPEG.dami0804/KakaoTalk_20220818_204635670.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222851965374"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title7",
+                "https://postfiles.pstatic.net/MjAyMjA4MThfNzgg/MDAxNjYwODIzMzc4MDE2.7KCuc0VAyGWrNIyplzMXGFif_L51isyCf4X6bm6Hqm0g.T3gpzQCnyvBc59Om1ygVQrDlCIlRUXYH4Annjc_202cg.JPEG.dami0804/KakaoTalk_20220818_204635670_01.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222851965669"
+            )
+        )
+        myRef.push().setValue(
+            ContentModel(
+                "title8",
+                "https://postfiles.pstatic.net/MjAyMjA4MThfNTIg/MDAxNjYwODIzNDE1MjEz.8YEfMbF3BfEW1HpwQuv52ysY5PnJ9mLogJiCmfMOFDMg.MYT-CZ4b-CsKP2sH3o0yKRU44fsyRmOAe2gS6cAVxPgg.JPEG.dami0804/KakaoTalk_20220818_204635670_02.jpg?type=w773",
+                "https://blog.naver.com/dami0804/222851966072"
+            )
         )*/
 
+
         val rv: RecyclerView = findViewById(R.id.rv)
-        val items = ArrayList<ContentModel>()
+
         // items.add(ContentModel("imageurl","title","weburl"))
-        items.add(
+       /* items.add(
             ContentModel(
                 "title1",
                 "https://postfiles.pstatic.net/MjAyMjA4MTdfMjMz/MDAxNjYwNzI3Mjg1NjE5.ZX4YzSh_sfNojjZ-Rj_V-q7F1VMz52eBsAZ-D2c5kNEg.XTvQeMwH4PwtkUklpOflZaW1GAt-Rc8QqX6I0r4dCt4g.JPEG.dami0804/KakaoTalk_20220817_180616358.jpg?type=w773",
@@ -87,7 +163,7 @@ class ContentListActivity : AppCompatActivity() {
                 "https://postfiles.pstatic.net/MjAyMjA4MThfNTIg/MDAxNjYwODIzNDE1MjEz.8YEfMbF3BfEW1HpwQuv52ysY5PnJ9mLogJiCmfMOFDMg.MYT-CZ4b-CsKP2sH3o0yKRU44fsyRmOAe2gS6cAVxPgg.JPEG.dami0804/KakaoTalk_20220818_204635670_02.jpg?type=w773",
                 "https://blog.naver.com/dami0804/222851966072"
             )
-        )
+        )*/
 
 
 
@@ -119,8 +195,10 @@ class ContentListActivity : AppCompatActivity() {
         val share = hashMapOf(
             "kakaoUserId" to userId)
 
-        addShareData("memo", share)
-       }  //DB에 데이터 추가
+       /* addShareData("memo", share)*/
+       }
+
+    //DB에 데이터 추가
     private fun addShareData(collectionPath: String, share: HashMap<String, Long>) {
         //DB에 문서 추가 -
         db.collection(collectionPath)
