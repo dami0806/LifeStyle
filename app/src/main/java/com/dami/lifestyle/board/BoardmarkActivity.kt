6671683a52a080529cfe69ba6f29bbd7019eeb88
dami <dami0804@naver.com>
2022-09-06 00:31:16 +0900

    파일추가

---
+++ app/src/main/java/com/dami/lifestyle/board/BoardmarkActivity.kt
@@ -0,0 +1,12 @@
+package com.dami.lifestyle.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dami.lifestyle.R
+
+class BoardmarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boardmark)
+    }
+}
---
+++ app/src/main/java/com/dami/lifestyle/board/BoardmarkModel.kt
@@ -0,0 +1,5 @@
+package com.dami.lifestyle.board
+
+class BoardmarkModel {
+    val BoardmarkIsTrue: Boolean?=null
+}
